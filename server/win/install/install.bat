9a30e0baf6ae5d41d1fa7d23ce752b746c35b0d2
Isaiah Choe <choe.jungyeon@navercorp.com>
2017-06-23 12:28:23 +0900

    remove webmanager (#15)
    * remove webmanager app
    
    - delete source codes in share/webmanager
    
    * remove webmanager app
    
    - removed webmanager relations in build scripts and configuration file
    - removed checking logic of webmanager path in cm_config.cpp
    
    * remove webmanager app
    
    - update version number

--- server/win/install/install.bat
+++ server/win/install/install.bat
@@ -10,7 +10,6 @@ set exitcode=0
 mkdir %DEST_DIR%
 mkdir %DEST_DIR%\bin
 mkdir %DEST_DIR%\conf
-mkdir %DEST_DIR%\share
 
 copy %SRC_DIR%\*.exe %DEST_DIR%\bin
 copy %SRC_DIR%\*.pdb %DEST_DIR%\bin
@@ -18,7 +17,7 @@ copy %SRC_DIR%\*.pdb %DEST_DIR%\bin
 copy %SRC_DIR%\..\..\cmserver\conf\*.conf %DEST_DIR%\conf
 copy %SRC_DIR%\..\..\cmserver\conf\*.pass %DEST_DIR%\conf
 copy %SRC_DIR%\..\..\cmserver\conf\cm_ssl_cert.* %DEST_DIR%\conf
-robocopy %SRC_DIR%\..\..\share %DEST_DIR%\share /MIR
+
 if errorlevel 1 (
 	exit /b 0
 	)
