a91c4ac9a0056c9f787919c7eaad76ae5e1a6db7
ESeok Oh <eseok.oh@navercorp.com>
2019-04-04 16:25:15 +0900

    [CBRD-22855] fix compile errors of gcc7 (#26)
    http://jira.cubrid.org/browse/CBRD-22855

--- server/src/cm_user.cpp
+++ server/src/cm_user.cpp
@@ -74,7 +74,7 @@ dbmt_user_read (T_DBMT_USER *dbmt_user, char *_dbmt_error)
   if (fp == NULL)
     {
       strcpy (_dbmt_error,
-              conf_get_dbmt_file2 (FID_DBMT_CUBRID_PASS, strbuf));
+	      conf_get_dbmt_file2 (FID_DBMT_CUBRID_PASS, strbuf));
       retval = ERR_FILE_OPEN_FAIL;
       goto read_dbmt_user_error;
     }
@@ -84,141 +84,143 @@ dbmt_user_read (T_DBMT_USER *dbmt_user, char *_dbmt_error)
     {
       ut_trim (strbuf);
       if (strncmp (strbuf, CUBRID_PASS_OPEN_TAG, CUBRID_PASS_OPEN_TAG_LEN) == 0)
-        {
-          snprintf (cur_user, sizeof (cur_user) - 1, "%s", strbuf + CUBRID_PASS_OPEN_TAG_LEN);
-          if (cur_user[0] == '\0')
-            {
-              continue;
-            }
+	{
+	  snprintf (cur_user, sizeof (cur_user) - 1, "%s", strbuf + CUBRID_PASS_OPEN_TAG_LEN);
+	  if (cur_user[0] == '\0')
+	    {
+	      continue;
+	    }
 
-          user_info =
-            (T_DBMT_USER_INFO *) increase_capacity (user_info,
-                sizeof (T_DBMT_USER_INFO), num_dbmt_user,
-                num_dbmt_user + 1);
-          if (user_info == NULL)
-            {
-              retval = ERR_MEM_ALLOC;
-              goto read_dbmt_user_error;
-            }
-          num_dbmt_user++;
+	  user_info =
+		  (T_DBMT_USER_INFO *) increase_capacity (user_info,
+		      sizeof (T_DBMT_USER_INFO), num_dbmt_user,
+		      num_dbmt_user + 1);
+	  if (user_info == NULL)
+	    {
+	      retval = ERR_MEM_ALLOC;
+	      goto read_dbmt_user_error;
+	    }
+	  num_dbmt_user++;
 
-          /* user name set */
-          strcpy_limit (user_info[num_dbmt_user - 1].user_name, cur_user,
-                        DBMT_USER_NAME_LEN);
-          if (user_dbinfo != NULL)
-            {
-              free (user_dbinfo);
-              user_dbinfo = NULL;
-            }
-          num_dbinfo = 0;
-          num_authinfo = 0;
-          continue;
-        }
+	  /* user name set */
+	  strcpy_limit (user_info[num_dbmt_user - 1].user_name, cur_user,
+			DBMT_USER_NAME_LEN);
+	  if (user_dbinfo != NULL)
+	    {
+	      free (user_dbinfo);
+	      user_dbinfo = NULL;
+	    }
+	  num_dbinfo = 0;
+	  num_authinfo = 0;
+	  continue;
+	}
 
       if (cur_user[0] == '\0')
-        {
-          continue;
-        }
+	{
+	  continue;
+	}
 
       if (strncmp (strbuf, CUBRID_PASS_CLOSE_TAG, CUBRID_PASS_CLOSE_TAG_LEN) == 0)
-        {
-          if (strcmp (strbuf + CUBRID_PASS_CLOSE_TAG_LEN, cur_user) != 0)
-            {
-              strcpy (_dbmt_error, conf_get_dbmt_file2 (FID_DBMT_CUBRID_PASS,
-                      strbuf));
-              retval = ERR_FILE_INTEGRITY;
-              goto read_dbmt_user_error;
-            }
-          if (user_info != NULL)
-            {
-              user_info[num_dbmt_user - 1].dbinfo = user_dbinfo;
-              user_dbinfo = NULL;
-              user_info[num_dbmt_user - 1].num_dbinfo = num_dbinfo;
+	{
+	  if (strcmp (strbuf + CUBRID_PASS_CLOSE_TAG_LEN, cur_user) != 0)
+	    {
+	      strcpy (_dbmt_error, conf_get_dbmt_file2 (FID_DBMT_CUBRID_PASS,
+		      strbuf));
+	      retval = ERR_FILE_INTEGRITY;
+	      goto read_dbmt_user_error;
+	    }
+	  if (user_info != NULL)
+	    {
+	      user_info[num_dbmt_user - 1].dbinfo = user_dbinfo;
+	      user_dbinfo = NULL;
+	      user_info[num_dbmt_user - 1].num_dbinfo = num_dbinfo;
 
-              user_info[num_dbmt_user - 1].authinfo = user_authinfo;
-              user_authinfo = NULL;
-              user_info[num_dbmt_user - 1].num_authinfo = num_authinfo;
-            }
-          cur_user[0] = '\0';
-        }
+	      user_info[num_dbmt_user - 1].authinfo = user_authinfo;
+	      user_authinfo = NULL;
+	      user_info[num_dbmt_user - 1].num_authinfo = num_authinfo;
+	    }
+	  cur_user[0] = '\0';
+	}
       else
-        {
-          /* get authority info and database info */
-          if (string_tokenize2 (strbuf, authinfo, 2, ':') < 0)
-            {
-              continue;
-            }
-          else
-            {
-              if ((strcmp (authinfo[0], "dbc") == 0)
-                  || (strcmp (authinfo[0], "dbo") == 0)
-                  || (strcmp (authinfo[0], "brk") == 0)
-                  || (strcmp (authinfo[0], "mon") == 0)
-                  || (strcmp (authinfo[0], "job") == 0)
-                  || (strcmp (authinfo[0], "var") == 0)
-                  || (strcmp (authinfo[0], "user_auth") == 0)
-                  || (strcmp (authinfo[0], "admin") == 0) ||
-                  // for old cmdb.pass file.
-                  (strcmp (authinfo[0], "unicas") == 0)
-                  || (strcmp (authinfo[0], "dbcreate") == 0)
-                  || (strcmp (authinfo[0], "statusmonitorauth") == 0))
-                {
-                  T_DBMT_USER_AUTHINFO tmp_authinfo;
-                  memset (&tmp_authinfo, 0, sizeof (T_DBMT_USER_AUTHINFO));
+	{
+	  /* get authority info and database info */
+	  if (string_tokenize2 (strbuf, authinfo, 2, ':') < 0)
+	    {
+	      continue;
+	    }
+	  else
+	    {
+	      if ((strcmp (authinfo[0], "dbc") == 0)
+		  || (strcmp (authinfo[0], "dbo") == 0)
+		  || (strcmp (authinfo[0], "brk") == 0)
+		  || (strcmp (authinfo[0], "mon") == 0)
+		  || (strcmp (authinfo[0], "job") == 0)
+		  || (strcmp (authinfo[0], "var") == 0)
+		  || (strcmp (authinfo[0], "user_auth") == 0)
+		  || (strcmp (authinfo[0], "admin") == 0) ||
+		  // for old cmdb.pass file.
+		  (strcmp (authinfo[0], "unicas") == 0)
+		  || (strcmp (authinfo[0], "dbcreate") == 0)
+		  || (strcmp (authinfo[0], "statusmonitorauth") == 0))
+		{
+		  T_DBMT_USER_AUTHINFO tmp_authinfo;
+		  memset (&tmp_authinfo, 0, sizeof (T_DBMT_USER_AUTHINFO));
 
-                  dbmt_user_set_authinfo (&tmp_authinfo, authinfo[0], authinfo[1]);
+		  dbmt_user_set_authinfo (&tmp_authinfo, authinfo[0], authinfo[1]);
 
-                  user_authinfo =
-                    (T_DBMT_USER_AUTHINFO *) increase_capacity (user_authinfo,
-                        sizeof (T_DBMT_USER_AUTHINFO),
-                        num_authinfo, num_authinfo + 1);
-                  if (user_authinfo == NULL)
-                    {
-                      retval = ERR_MEM_ALLOC;
-                      goto read_dbmt_user_error;
-                    }
-                  num_authinfo++;
-                  user_authinfo[num_authinfo - 1] = tmp_authinfo;
-                }
-              else
-                {
-                  T_DBMT_USER_DBINFO tmp_dbinfo;
-                  memset (&tmp_dbinfo, 0, sizeof (T_DBMT_USER_DBINFO));
+		  user_authinfo =
+			  (T_DBMT_USER_AUTHINFO *) increase_capacity (user_authinfo,
+			      sizeof (T_DBMT_USER_AUTHINFO),
+			      num_authinfo, num_authinfo + 1);
+		  if (user_authinfo == NULL)
+		    {
+		      retval = ERR_MEM_ALLOC;
+		      goto read_dbmt_user_error;
+		    }
+		  num_authinfo++;
+		  user_authinfo[num_authinfo - 1] = tmp_authinfo;
+		}
+	      else
+		{
+		  T_DBMT_USER_DBINFO tmp_dbinfo;
+		  memset (&tmp_dbinfo, 0, sizeof (T_DBMT_USER_DBINFO));
 
-                  if (string_tokenize2 (authinfo[1], dbinfo, 3, ';') < 0)
-                    {
-                      continue;
-                    }
+		  if (string_tokenize2 (authinfo[1], dbinfo, 3, ';') < 0)
+		    {
+		      continue;
+		    }
 
-                  /* 8.3.0 cmdb.pass not store dbpasswd. For compatibility,
-                  * check if use older version cmdb.pass */
-                  if ((dbinfo[3] = strchr (dbinfo[2], ';')) != NULL)
-                    {
-                      dbinfo[3] = '\0';
-                      dbinfo[3]++;
-                      dbmt_user_set_dbinfo (&tmp_dbinfo, authinfo[0],
-                                            dbinfo[0], dbinfo[1], dbinfo[3]);
-                    }
-                  else
-                    {
-                      dbmt_user_set_dbinfo (&tmp_dbinfo, authinfo[0],
-                                            dbinfo[0], dbinfo[1], dbinfo[2]);
-                    }
+		  /* 8.3.0 cmdb.pass not store dbpasswd. For compatibility,
+		  * check if use older version cmdb.pass */
+		  char *separator;
+		  separator = dbinfo[3] = strchr (dbinfo[2], ';');
+		  if (separator != NULL)
+		    {
+		      *separator = '\0';
+		      dbinfo[3] = separator + 1;
+		      dbmt_user_set_dbinfo (&tmp_dbinfo, authinfo[0],
+					    dbinfo[0], dbinfo[1], dbinfo[3]);
+		    }
+		  else
+		    {
+		      dbmt_user_set_dbinfo (&tmp_dbinfo, authinfo[0],
+					    dbinfo[0], dbinfo[1], dbinfo[2]);
+		    }
 
-                  user_dbinfo =
-                    (T_DBMT_USER_DBINFO *) increase_capacity (user_dbinfo,
-                        sizeof (T_DBMT_USER_DBINFO),
-                        num_dbinfo, num_dbinfo + 1);
-                  if (user_dbinfo == NULL)
-                    {
-                      retval = ERR_MEM_ALLOC;
-                      goto read_dbmt_user_error;
-                    }
-                  num_dbinfo++;
-                  user_dbinfo[num_dbinfo - 1] = tmp_dbinfo;
-                }
-            }
-        }
+		  user_dbinfo =
+			  (T_DBMT_USER_DBINFO *) increase_capacity (user_dbinfo,
+			      sizeof (T_DBMT_USER_DBINFO),
+			      num_dbinfo, num_dbinfo + 1);
+		  if (user_dbinfo == NULL)
+		    {
+		      retval = ERR_MEM_ALLOC;
+		      goto read_dbmt_user_error;
+		    }
+		  num_dbinfo++;
+		  user_dbinfo[num_dbinfo - 1] = tmp_dbinfo;
+		}
+	    }
+	}
     }
   fclose (fp);
   fp = NULL;
@@ -237,7 +239,7 @@ dbmt_user_read (T_DBMT_USER *dbmt_user, char *_dbmt_error)
   if (num_dbmt_user < 1)
     {
       strcpy (_dbmt_error,
-              conf_get_dbmt_file2 (FID_DBMT_CUBRID_PASS, strbuf));
+	      conf_get_dbmt_file2 (FID_DBMT_CUBRID_PASS, strbuf));
       retval = ERR_FILE_INTEGRITY;
       goto read_dbmt_user_error;
     }
@@ -260,19 +262,19 @@ dbmt_user_read (T_DBMT_USER *dbmt_user, char *_dbmt_error)
 
       ut_trim (strbuf);
       if (string_tokenize2 (strbuf, tok, 2, ':') < 0)
-        {
-          continue;
-        }
+	{
+	  continue;
+	}
       for (i = 0; i < dbmt_user->num_dbmt_user; i++)
-        {
-          if (strcmp (tok[0], dbmt_user->user_info[i].user_name) == 0)
-            {
-              snprintf (dbmt_user->user_info[i].user_passwd,
-                        sizeof (dbmt_user->user_info[i].user_passwd) - 1,
-                        "%s", tok[1]);
-              break;
-            }
-        }
+	{
+	  if (strcmp (tok[0], dbmt_user->user_info[i].user_name) == 0)
+	    {
+	      snprintf (dbmt_user->user_info[i].user_passwd,
+			sizeof (dbmt_user->user_info[i].user_passwd) - 1,
+			"%s", tok[1]);
+	      break;
+	    }
+	}
     }
   fclose (fp);
 
@@ -310,16 +312,16 @@ dbmt_user_free (T_DBMT_USER *dbmt_user)
   if (dbmt_user->user_info)
     {
       for (i = 0; i < dbmt_user->num_dbmt_user; i++)
-        {
-          if (dbmt_user->user_info[i].dbinfo)
-            {
-              free (dbmt_user->user_info[i].dbinfo);
-            }
-          if (dbmt_user->user_info[i].authinfo)
-            {
-              free (dbmt_user->user_info[i].authinfo);
-            }
-        }
+	{
+	  if (dbmt_user->user_info[i].dbinfo)
+	    {
+	      free (dbmt_user->user_info[i].dbinfo);
+	    }
+	  if (dbmt_user->user_info[i].authinfo)
+	    {
+	      free (dbmt_user->user_info[i].authinfo);
+	    }
+	}
       free (dbmt_user->user_info);
     }
 }
@@ -346,35 +348,35 @@ dbmt_user_write_auth (T_DBMT_USER *dbmt_user, char *_dbmt_error)
   for (i = 0; i < dbmt_user->num_dbmt_user; i++)
     {
       if (dbmt_user->user_info[i].user_name[0] == '\0')
-        {
-          continue;
-        }
+	{
+	  continue;
+	}
 
       fprintf (fp, "%s%s\n", CUBRID_PASS_OPEN_TAG,
-               dbmt_user->user_info[i].user_name);
+	       dbmt_user->user_info[i].user_name);
       for (j = 0; j < dbmt_user->user_info[i].num_authinfo; j++)
-        {
-          if (dbmt_user->user_info[i].authinfo[j].domain[0] == '\0')
-            {
-              continue;
-            }
-          fprintf (fp, "%s:%s\n", dbmt_user->user_info[i].authinfo[j].domain,
-                   dbmt_user->user_info[i].authinfo[j].auth);
-        }
+	{
+	  if (dbmt_user->user_info[i].authinfo[j].domain[0] == '\0')
+	    {
+	      continue;
+	    }
+	  fprintf (fp, "%s:%s\n", dbmt_user->user_info[i].authinfo[j].domain,
+		   dbmt_user->user_info[i].authinfo[j].auth);
+	}
       for (j = 0; j < dbmt_user->user_info[i].num_dbinfo; j++)
-        {
-          if (dbmt_user->user_info[i].dbinfo[j].dbname[0] == '\0')
-            {
-              continue;
-            }
-          fprintf (fp, "%s:%s;%s;%s\n",
-                   dbmt_user->user_info[i].dbinfo[j].dbname,
-                   dbmt_user->user_info[i].dbinfo[j].auth,
-                   dbmt_user->user_info[i].dbinfo[j].uid,
-                   dbmt_user->user_info[i].dbinfo[j].broker_address);
-        }
+	{
+	  if (dbmt_user->user_info[i].dbinfo[j].dbname[0] == '\0')
+	    {
+	      continue;
+	    }
+	  fprintf (fp, "%s:%s;%s;%s\n",
+		   dbmt_user->user_info[i].dbinfo[j].dbname,
+		   dbmt_user->user_info[i].dbinfo[j].auth,
+		   dbmt_user->user_info[i].dbinfo[j].uid,
+		   dbmt_user->user_info[i].dbinfo[j].broker_address);
+	}
       fprintf (fp, "%s%s\n", CUBRID_PASS_CLOSE_TAG,
-               dbmt_user->user_info[i].user_name);
+	       dbmt_user->user_info[i].user_name);
     }
   fclose (fp);
 
@@ -392,8 +394,8 @@ dbmt_user_write_auth (T_DBMT_USER *dbmt_user, char *_dbmt_error)
 
 void
 dbmt_user_set_dbinfo (T_DBMT_USER_DBINFO *dbinfo, const char *dbname,
-                      const char *auth, const char *uid,
-                      const char *broker_address)
+		      const char *auth, const char *uid,
+		      const char *broker_address)
 {
   strcpy_limit (dbinfo->dbname, dbname, sizeof (dbinfo->dbname));
   strcpy_limit (dbinfo->auth, auth, sizeof (dbinfo->auth));
@@ -405,13 +407,13 @@ dbmt_user_set_dbinfo (T_DBMT_USER_DBINFO *dbinfo, const char *dbname,
   else
     {
       strcpy_limit (dbinfo->broker_address, broker_address,
-                    sizeof (dbinfo->broker_address));
+		    sizeof (dbinfo->broker_address));
     }
 }
 
 void
 dbmt_user_set_authinfo (T_DBMT_USER_AUTHINFO *authinfo, const char *domain,
-                        const char *auth)
+			const char *auth)
 {
   strcpy_limit (authinfo->domain, domain, sizeof (authinfo->domain));
   strcpy_limit (authinfo->auth, auth, sizeof (authinfo->auth));
@@ -419,9 +421,9 @@ dbmt_user_set_authinfo (T_DBMT_USER_AUTHINFO *authinfo, const char *domain,
 
 void
 dbmt_user_set_userinfo (T_DBMT_USER_INFO *usrinfo, const char *user_name,
-                        const char *user_passwd, int num_authinfo,
-                        T_DBMT_USER_AUTHINFO *authinfo, int num_dbinfo,
-                        T_DBMT_USER_DBINFO *dbinfo)
+			const char *user_passwd, int num_authinfo,
+			T_DBMT_USER_AUTHINFO *authinfo, int num_dbinfo,
+			T_DBMT_USER_DBINFO *dbinfo)
 {
   strcpy_limit (usrinfo->user_name, user_name, sizeof (usrinfo->user_name));
   strcpy_limit (usrinfo->user_passwd, user_passwd, sizeof (usrinfo->user_passwd));
@@ -439,9 +441,9 @@ dbmt_user_search (T_DBMT_USER_INFO *user_info, const char *dbname)
   for (i = 0; i < user_info->num_dbinfo; i++)
     {
       if (strcmp (user_info->dbinfo[i].dbname, dbname) == 0)
-        {
-          return i;
-        }
+	{
+	  return i;
+	}
     }
 
   return -1;
@@ -468,11 +470,11 @@ dbmt_user_write_pass (T_DBMT_USER *dbmt_user, char *_dbmt_error)
   for (i = 0; i < dbmt_user->num_dbmt_user; i++)
     {
       if (dbmt_user->user_info[i].user_name[0] == '\0')
-        {
-          continue;
-        }
+	{
+	  continue;
+	}
       fprintf (fp, "%s:%s\n", dbmt_user->user_info[i].user_name,
-               dbmt_user->user_info[i].user_passwd);
+	       dbmt_user->user_info[i].user_passwd);
     }
   fclose (fp);
 
@@ -496,12 +498,12 @@ dbmt_user_db_delete (T_DBMT_USER *dbmt_user, char *dbname)
   for (i = 0; i < dbmt_user->num_dbmt_user; i++)
     {
       for (j = 0; j < dbmt_user->user_info[i].num_dbinfo; j++)
-        {
-          if (strcmp (dbmt_user->user_info[i].dbinfo[j].dbname, dbname) == 0)
-            {
-              dbmt_user->user_info[i].dbinfo[j].dbname[0] = '\0';
-            }
-        }
+	{
+	  if (strcmp (dbmt_user->user_info[i].dbinfo[j].dbname, dbname) == 0)
+	    {
+	      dbmt_user->user_info[i].dbinfo[j].dbname[0] = '\0';
+	    }
+	}
     }
 }
 
@@ -511,9 +513,9 @@ dbmt_user_add_dbinfo (T_DBMT_USER_INFO *usrinfo, T_DBMT_USER_DBINFO *dbinfo)
   int i = usrinfo->num_dbinfo;
 
   usrinfo->dbinfo =
-    (T_DBMT_USER_DBINFO *) increase_capacity (usrinfo->dbinfo,
-        sizeof (T_DBMT_USER_DBINFO),
-        i, i + 1);
+	  (T_DBMT_USER_DBINFO *) increase_capacity (usrinfo->dbinfo,
+	      sizeof (T_DBMT_USER_DBINFO),
+	      i, i + 1);
   if (usrinfo->dbinfo == NULL)
     {
       return ERR_MEM_ALLOC;
@@ -531,14 +533,14 @@ dbmt_user_add_dbinfo (T_DBMT_USER_INFO *usrinfo, T_DBMT_USER_DBINFO *dbinfo)
 
 int
 dbmt_user_new_token_info (const char *user_id,
-                          const char *user_ip,
-                          const char *user_port, const char *token_enc,
+			  const char *user_ip,
+			  const char *user_port, const char *token_enc,
 #ifdef WINDOWS
-                          HANDLE proc_id,
+			  HANDLE proc_id,
 #else
-                          pid_t proc_id,
+			  pid_t proc_id,
 #endif
-                          time_t login_time)
+			  time_t login_time)
 {
   T_USER_TOKEN_INFO *end_node;
   T_USER_TOKEN_INFO *new_node;
@@ -547,18 +549,18 @@ dbmt_user_new_token_info (const char *user_id,
     {
       new_node = (T_USER_TOKEN_INFO *) malloc (sizeof (T_USER_TOKEN_INFO));
       if (new_node == NULL)
-        {
-          return ERR_MEM_ALLOC;
-        }
+	{
+	  return ERR_MEM_ALLOC;
+	}
 
       if (user_token_info == NULL)
-        {
-          end_node = user_token_info = new_node;
-        }
+	{
+	  end_node = user_token_info = new_node;
+	}
       else
-        {
-          end_node = user_token_info->prev;
-        }
+	{
+	  end_node = user_token_info->prev;
+	}
       end_node->next = new_node;
       new_node->next = user_token_info;
       new_node->prev = end_node;
@@ -594,9 +596,9 @@ dbmt_user_search_token_info (const char *user_id)
   do
     {
       if (!strcmp (head->user_id, user_id))
-        {
-          return head;
-        }
+	{
+	  return head;
+	}
 
       head = head->next;
 
@@ -625,9 +627,9 @@ dbmt_user_search_token_info_by_token (const char *token)
   do
     {
       if (!strcmp (head->token, token))
-        {
-          return head;
-        }
+	{
+	  return head;
+	}
 
       head = head->next;
 
@@ -657,13 +659,13 @@ dbmt_user_delete_token_info (const char *user_id)
   if (removed_node == user_token_info)
     {
       if (user_token_info->next != user_token_info)
-        {
-          user_token_info = user_token_info->next;
-        }
+	{
+	  user_token_info = user_token_info->next;
+	}
       else
-        {
-          user_token_info = NULL;
-        }
+	{
+	  user_token_info = NULL;
+	}
     }
 
   removed_node->prev->next = removed_node->next;
@@ -691,13 +693,13 @@ dbmt_user_delete_token_info_by_token (const char *token)
   if (removed_node == user_token_info)
     {
       if (user_token_info->next != user_token_info)
-        {
-          user_token_info = user_token_info->next;
-        }
+	{
+	  user_token_info = user_token_info->next;
+	}
       else
-        {
-          user_token_info = NULL;
-        }
+	{
+	  user_token_info = NULL;
+	}
 
     }
 
