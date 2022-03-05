99ee88467030c53f1e210d2f1d5d3cae96d49070
Kisoo Han <kisoo-han@cubrid.com>
2022-03-05 22:22:40 +0900

    [CBRD-24221] Display filename failed to open

--- server/src/cm_job_task.cpp
+++ server/src/cm_job_task.cpp
@@ -1662,6 +1662,7 @@ ts_get_all_sysparam (nvplist *req, nvplist *res, char *_dbmt_error)
 
   if (access (conf_path, F_OK) < 0)
     {
+      strcpy (_dbmt_error, conf_path);
       return ERR_FILE_OPEN_FAIL;
     }
 
