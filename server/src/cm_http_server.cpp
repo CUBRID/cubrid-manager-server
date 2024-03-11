b10ef0206b5b994a851b245aeedca067a5df3f78
Kisoo Han <kisoo-han@cubrid.com>
2024-03-11 13:56:53 +0800

    [CBRD-25232] 11.0, exclude Win for TLS_v1.2 (#94)
    exclude Win for TLS_v1.2

--- server/src/cm_http_server.cpp
+++ server/src/cm_http_server.cpp
@@ -579,7 +579,13 @@ SSL_CTX *init_SSL (const char *certificate_chain,const char *private_key)
 
 
   /* Currently, we support upto TLS_v1.2 */
+
+#if !defined (WINDOWS)
   ctx = SSL_CTX_new (TLS_server_method ());
+#else
+  ctx = SSL_CTX_new (TLSv1_server_method ());
+#endif
+
   if (!ctx)
     {
       LOG_ERROR ("-- Web server: Fail to generate CTX for openSSL.");
