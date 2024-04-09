296dd62d01002b80fa50fa9c919a4f4bea83298e
Kisoo Han <kisoo-han@cubrid.com>
2024-04-10 01:04:11 +0900

    [CBRD-25295] start all brokers

--- server/src/cm_job_task.cpp
+++ server/src/cm_job_task.cpp
@@ -1035,11 +1035,21 @@ ts2_get_unicas_info (nvplist *in, nvplist *out, char *_dbmt_error)
   return ERR_NO_ERROR;
 }
 
-
 int
 ts2_start_unicas (nvplist *in, nvplist *out, char *_dbmt_error)
 {
   T_CM_ERROR error;
+
+#if !defined (WINDOWS)
+  const char *argv[3];
+  char cmd_name[CUBRID_CMD_NAME_LEN];
+  char cubrid_err_file[PATH_MAX];
+  int argc = 0;
+  int retval = ERR_NO_ERROR;
+  int rc;
+#endif
+
+#if defined (WINDOWS)
   if (cm_broker_env_start (&error) < 0)
     {
       strcpy (_dbmt_error, error.err_msg);
@@ -1047,6 +1057,28 @@ ts2_start_unicas (nvplist *in, nvplist *out, char *_dbmt_error)
     }
 
   return ERR_NO_ERROR;
+#else
+#if !defined (DO_NOT_USE_CUBRIDENV)
+  sprintf (cmd_name, "%s/%s%s", sco.szCubrid, CUBRID_DIR_BIN, UTIL_BROKER_NAME);
+#else
+  sprintf (cmd_name, "%s/%s", CUBRID_BINDIR, UTIL_BROKER_NAME);
+#endif
+  argv[argc++] = cmd_name;
+  argv[argc++] = "start";
+  argv[argc++] = NULL;
+
+  make_temp_filepath (cubrid_err_file, sco.dbmt_tmp_dir, "broker_start", TS2_STARTBROKER, PATH_MAX);
+  if (run_child (argv, 1, NULL, NULL, cubrid_err_file, &rc) < 0 || rc != 0)
+    {
+      if (read_error_file (cubrid_err_file, _dbmt_error, -1) < 0)
+        {
+          retval = ERR_WITH_MSG;
+        }
+    }
+
+  unlink (cubrid_err_file);
+  return retval;
+#endif
 }
 
 int
