02126d3128fc288c966977b06b449524d1dd134b
Kisoo Han <kisoo-han@cubrid.com>
2026-03-13 08:45:17 +0900

    [CBRD-26542] rsolve syntax error, missing semicolon

--- server/src/cm_job_task.cpp
+++ server/src/cm_job_task.cpp
@@ -3172,7 +3172,7 @@ tsRenameDB (nvplist *req, nvplist *res, char *_dbmt_error)
 	    {
 	      if (v == NULL)
 		{
-		  strcpy (_dbmt_error, "invalid volume parameters")
+		  strcpy (_dbmt_error, "invalid volume parameters");
 		  return ERR_WITH_MSG;
 		}
 #if defined(WINDOWS)
