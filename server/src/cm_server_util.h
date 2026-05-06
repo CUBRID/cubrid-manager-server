6e8dcaa9e3b9d580ed6696693222bb7ffdb600bc
Kisoo Han <kisoo-han@cubrid.com>
2026-05-06 11:47:37 +0900

    [CBRD-26543] remove unused function, run_child_linux ()

--- server/src/cm_server_util.h
+++ server/src/cm_server_util.h
@@ -211,9 +211,6 @@ int ut_get_host_stat (T_CMS_HOST_STAT *stat, char *_dbmt_error);
 int ut_get_proc_stat (T_CMS_PROC_STAT *stat, int pid);
 int ut_record_cubrid_utility_log_stderr (const char *msg);
 int ut_record_cubrid_utility_log_stdout (const char *msg);
-int run_child_linux (const char *pname, const char *const argv[], int wait_flag,
-                     const char *stdin_file, char *stdout_file, char *stderr_file,
-                     int *exit_status);
 void write_manager_access_log (const char *protocol_str, const char *msg);
 void write_manager_error_log (const char *protocol_str, const char *msg);
 
