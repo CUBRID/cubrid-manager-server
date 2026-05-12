bf4fd5ecc0462201d27a42f7c78e36175c2bc024
Kisoo Han <kisoo-han@cubrid.com>
2026-05-12 16:24:00 +0900

    [CBRD-26777] check valid filename for renamedb API

--- server/src/cm_job_task.cpp
+++ server/src/cm_job_task.cpp
@@ -3134,6 +3134,12 @@ tsRenameDB (nvplist *req, nvplist *res, char *_dbmt_error)
       return ERR_PARAM_MISSING;
     }
 
+  if (is_invalid_filename (exvolpath))
+    {
+      ERR_FILENAME_NOT_ALLOWED (_dbmt_error, exvolpath);
+      return ERR_WITH_MSG;
+    }
+
   db_mode = uDatabaseMode (dbname, NULL);
   if (db_mode == DB_SERVICE_MODE_SA)
     {
