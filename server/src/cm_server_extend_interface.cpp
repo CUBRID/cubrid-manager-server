599533df55789caa6a76b3d00a53844d3406a096
Kisoo Han <kisoo-han@cubrid.com>
2026-07-15 13:23:54 +0900

    [CBRD-26916] check invalid time format

--- server/src/cm_server_extend_interface.cpp
+++ server/src/cm_server_extend_interface.cpp
@@ -87,6 +87,7 @@ static T_EXTEND_TASK_INFO ext_task_info[] =
 };
 
 static int count_and_get_last_word (string str, char *word);
+static int is_invalid_time (char *str);
 
 static bool
 ext_get_id_from_token (const char *token, char token_content[][TOKEN_LENGTH+1])
@@ -1269,7 +1270,7 @@ int ext_set_autoexec_query (Json::Value &request, Json::Value &response)
   string conf_item[AUTOEXECQUERY_CONF_ENTRY_NUM];
   fstream conf_file;
   ofstream tmp_file;
-  char interval[TOKEN_LENGTH];
+  char detail2[TOKEN_LENGTH];
 
 
   autoexecquery_conf_file[0] = '\0';
@@ -1408,7 +1409,7 @@ int ext_set_autoexec_query (Json::Value &request, Json::Value &response)
       // details of period
       conf_item[6] = queryplan[index]["detail"].asString();
 
-      if (count_and_get_last_word (conf_item[6], interval) != 2)
+      if (count_and_get_last_word (conf_item[6], detail2) != 2)
 	{
           char tmp[DBMT_ERROR_MSG_SIZE];
           snprintf (tmp, DBMT_ERROR_MSG_SIZE-1, "Invalid time format in detail: %s", conf_item[6].c_str());
@@ -1416,13 +1417,24 @@ int ext_set_autoexec_query (Json::Value &request, Json::Value &response)
           return build_server_header (response, ERR_WITH_MSG, tmp);
 	}
 
-      if (interval[0] == 'i')
+      if (detail2[0] == 'i')
 	{
           char tmp[DBMT_ERROR_MSG_SIZE];
 
-	  if (!is_positive_number (&interval[1]))
+	  if (!is_positive_number (&detail2[1]))
 	    {
-	      snprintf (tmp, DBMT_ERROR_MSG_SIZE-1, "Invalid interval: %s", interval);
+	      snprintf (tmp, DBMT_ERROR_MSG_SIZE-1, "Invalid interval: %s", detail2);
+	      tmp_file.close();
+	      return build_server_header (response, ERR_WITH_MSG, tmp);
+	    }
+	}
+      else
+	{
+          char tmp[DBMT_ERROR_MSG_SIZE];
+
+	  if (is_invalid_time (detail2))
+	    {
+	      snprintf (tmp, DBMT_ERROR_MSG_SIZE-1, "Invalid time spec: %s", detail2);
 	      tmp_file.close();
 	      return build_server_header (response, ERR_WITH_MSG, tmp);
 	    }
@@ -2749,4 +2761,25 @@ static int count_and_get_last_word (string str, char *word)
   snprintf (word, TOKEN_LENGTH, "%s", token.c_str ());
 
   return count;
+}
+
+static int is_invalid_time (char *str)
+{
+  int hour, minute;
+  char colon;
+  string timestr = str;
+
+  stringstream ss(timestr);
+
+  if (!(ss >> hour >> colon >> minute))
+    {
+      return 1;
+    }
+
+  if (!ss.eof() || colon != ':' || hour < 0 || hour > 23 || minute < 0 || minute > 59)
+    {
+        return 1;
+    }
+
+  return 0;
 }
