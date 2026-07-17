f678991350f58e19e927dbdcd4204e40f856f0b3
Kisoo Han <kisoo-han@cubrid.com>
2026-07-17 16:18:11 +0900

    [CBRD-26777] fix error for use of macro CUBRID_VERS ()

--- server/src/cm_job_task.cpp
+++ server/src/cm_job_task.cpp
@@ -4040,7 +4040,7 @@ ts_paramdump (nvplist *req, nvplist *res, char *_dbmt_error)
       argv[argc++] = "--" PARAMDUMP_BOTH_L;
     }
 
-  if (CUBRID_VERS (cubrid_version_major,cubrid_version_minor >= 1105))
+  if (CUBRID_VERS (cubrid_version_major,cubrid_version_minor) >= 1105)
     {
       argv[argc++] = "--" PLANDUMP_FOR_CM;
     }
@@ -12974,7 +12974,7 @@ _ts_lockdb_parse_us (nvplist *res, FILE *infile)
 	      nv_add_nvp (res, "numlocked", s1);
 
 	      fgets (buf, sizeof (buf), infile);
-	      if (CUBRID_VERS (cubrid_version_major,cubrid_version_minor < 1104))
+	      if (CUBRID_VERS (cubrid_version_major,cubrid_version_minor) < 1104)
 		{
 		  scan_matched =
 		      sscanf (buf, "%*s %*s %*s %*s %*s %*s %*s %*s %*s %255s", s2);
