181aec7d0a147efae8da047eebff99d488374cc7
Kisoo Han <kisoo-han@cubrid.com>
2026-07-17 17:37:51 +0900

    [CBRD-26777] add static keyword for delete_directory () win

--- server/src/cm_server_util.cpp
+++ server/src/cm_server_util.cpp
@@ -3747,7 +3747,8 @@ ut_record_cubrid_utility_log_stdout (const char *msg)
 }
 
 #if defined (WINDOWS)
-bool delete_directory (const std::string& rawPath)
+static bool
+delete_directory (const std::string& rawPath)
 {
   char abs_path[MAX_PATH + 1];
   if (GetFullPathNameA(rawPath.c_str(), MAX_PATH, abs_path, NULL) == 0)
