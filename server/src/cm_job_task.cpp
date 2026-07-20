811ed319391407c7ed599ec76172a7553ee95de3
Kisoo Han <kisoo-han@cubrid.com>
2026-07-20 20:22:21 +0900

    [CBRD-26777] fix compile error

--- server/src/cm_job_task.cpp
+++ server/src/cm_job_task.cpp
@@ -3697,7 +3697,7 @@ ts_copydb (nvplist *req, nvplist *res, char *_dbmt_error)
       return ERR_PARAM_MISSING;
     }
 
-  if (!is_authorized_filename (logpath, _dbmt_error) || !is_authorized_filename (destdbpath, _dbmt_error)
+  if (!is_authorized_filename (logpath, _dbmt_error) || !is_authorized_filename (destdbpath, _dbmt_error) ||
       !is_authorized_filename (exvolpath, _dbmt_error))
     {
       return ERR_WITH_MSG;
