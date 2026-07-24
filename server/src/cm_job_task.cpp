599affb539ef79e0fbbb66911cd3b129dcceda25
Kisoo Han <kisoo-han@cubrid.com>
2026-07-24 13:54:18 +0900

    [CBRD-26777] check filename auth for unloaddb

--- server/src/cm_job_task.cpp
+++ server/src/cm_job_task.cpp
@@ -4762,6 +4762,11 @@ ts_unloaddb (nvplist *req, nvplist *res, char *_dbmt_error)
       snprintf (fullpath, sizeof (fullpath) - 1, "%s", targetdir);
     }
 
+  if (!is_authorized_filename (fullpath, _dbmt_error))
+    {
+      return ERR_WITH_MSG;
+    }
+
   if (access (fullpath, F_OK) < 0)
     {
       if (uCreateDir (fullpath) != ERR_NO_ERROR)
@@ -4832,6 +4837,12 @@ ts_unloaddb (nvplist *req, nvplist *res, char *_dbmt_error)
     }
   argv[argc++] = "--" UNLOAD_OUTPUT_PATH_L;
   argv[argc++] = fullpath;
+
+  if (hashdir != NULL && *hashdir && !is_authorized_filename (hashdir, _dbmt_error))
+    {
+      return ERR_WITH_MSG;
+    }
+
   if ((usehash != NULL) && (strcmp (usehash, "yes") == 0))
     {
       argv[argc++] = "--" UNLOAD_HASH_FILE_L;
