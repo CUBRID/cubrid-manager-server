498f28af42471a01d3b56db67ebc2d6800de8e29
Kisoo Han <kisoo-han@cubrid.com>
2026-07-26 08:18:53 +0900

    [CBRD-26777] code style

--- server/src/cm_server_util.h
+++ server/src/cm_server_util.h
@@ -225,6 +225,6 @@ bool is_invalid_schema_file_lists (char *path, char *_dbmt_error);
 bool is_subpath (const char *allowd_path, const char *path);
 bool is_authorized_filename (const char *path, char *_dbmt_error);
 bool attempt_to_access_parent_dir (const char *path);
-bool is_allowed_script_env(const std::string& name);
+bool is_allowed_script_env (const std::string& name);
 std::string extract_env_name (const std::string& env_entry);
 #endif                /* _CM_SERVER_UTIL_H_ */
