21fb33deeac528cc08a1268d54fef8795bca70ce
Kisoo Han <kisoo-han@cubrid.com>
2026-07-31 22:17:10 +0900

    [CBRD-26777] move set async_param->status = 1 inside mutex

--- server/src/cm_server_interface.cpp
+++ server/src/cm_server_interface.cpp
@@ -487,12 +487,12 @@ cm_async_request_handler (void *lpArg)
       response["note"] = e.what ();
     }
 
-  async_param->status = 1;
   nv_destroy (cli_request);
   nv_destroy (cli_response);
 
 #ifndef WINDOWS
   pthread_mutex_lock (&async_param->mutex);
+  async_param->status = 1;
   if (async_param->is_timeout)
     {
       pthread_mutex_unlock (&async_param->mutex);
