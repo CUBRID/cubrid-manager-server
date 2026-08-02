cc2ea95d94e8d1992210a59bd5b40cdbcf25c709
Kisoo Han <kisoo-han@cubrid.com>
2026-08-02 16:29:47 +0900

    [CBRD-26777] move check logfilename point

--- server/src/cm_job_task.cpp
+++ server/src/cm_job_task.cpp
@@ -9731,11 +9731,6 @@ ts_executecasrunner (nvplist *cli_request, nvplist *cli_response,
   casrunnerwithFile = nv_get_val (cli_request, "executelogfile");
   logfilename = nv_get_val (cli_request, "logfile");
 
-  if (logfilename != NULL && !is_authorized_filename (logfilename, diag_error))
-    {
-      return ERR_WITH_MSG;
-    }
-
   if (brokername == NULL)
     {
       snprintf (diag_error, DBMT_ERROR_MSG_SIZE, "%s", "brokername");
@@ -9792,6 +9787,10 @@ ts_executecasrunner (nvplist *cli_request, nvplist *cli_response,
   if ((casrunnerwithFile != NULL) && (strcmp (casrunnerwithFile, "yes") == 0)
       && (logfilename != NULL))
     {
+      if (logfilename != NULL && !is_authorized_filename (logfilename, diag_error))
+	{
+	  return ERR_WITH_MSG;
+	}
       snprintf (tmplogfilename, PATH_MAX - 1, "%s", logfilename);
     }
   else
